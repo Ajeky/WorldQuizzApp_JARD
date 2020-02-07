a9dde4be9532e578f6a889e89b0ad0e3ebfc2586
Juan Manuel Gracia Pardal <jmgracia@AULAS.TRIANA>
2020-02-07 13:39:25 +0100

    Pregunta de fronteras casi resuelta

--- app/src/main/java/com/example/worldquizzapp_jard/services/PaisService.java
+++ app/src/main/java/com/example/worldquizzapp_jard/services/PaisService.java
@@ -5,13 +5,18 @@ import com.example.worldquizzapp_jard.models.Pais;
 
 import java.util.List;
 
+import lombok.Getter;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface PaisService {
 
     @GET("rest/v2/all/")
     Call<List<Pais>> listadoPaises();
+
+    @GET("rest/v2/alpha/{codigo}")
+    Call<Pais> nombrePais(@Path("codigo") String codigo);
 
 }
